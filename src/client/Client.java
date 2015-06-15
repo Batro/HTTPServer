5bdd332f36b4c6122925e1efb9acad7c1d025edf
p1409973 <p1409973@EPULWKS56.univ-lyon1.fr>
2015-06-15 11:44:42 +0200

    Awesome Commit

--- src/client/Client.java
+++ src/client/Client.java
@@ -20,11 +20,14 @@ public class Client {
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         System.out.println("Envoi de la string au serveur");
-        outToServer.writeBytes("GET / HTTP/1.1\n"
+        outToServer.writeBytes("GET /Koala.jpg HTTP/1.1\n"
                 + "Host: 127.0.0.1\n"
+                + "User-Agent: Mozilla/5.0 (Windows NT 6.1; WOW64; rv:24.0) Gecko/20100101 Firefox/24.0\n"
                 + "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\n"
                 + "Accept-Language: fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3\n"
-                + "Accept-Encoding: gzip, deflate\n");
+                + "Accept-Encoding: gzip, deflate\n"
+                + "Connection: keep-alive");
+        outToServer.flush();
         clientSocket.close();
     }
 }
